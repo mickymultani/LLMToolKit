7af781eb644660e135163fc3e15d32f952de6c0b
mickymult <micky.multani@gmail.com>
2023-10-13 21:45:05 -0600

    Added preprocessing and postprocessing modules

---
+++ src/llmtoolkit/data_processing/preprocessing.py
@@ -0,0 +1,40 @@
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+def preprocess_data(texts: List[str], remove_stopwords=True, lemmatize=True) -> List[str]:
+    """
+    Preprocess a list of texts.
+    """
+    processed_texts = []
+    
+    # Initialize lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    
+    # Stopwords
+    stop_words = set(stopwords.words('english'))
+
+    for text in texts:
+        # Convert to lowercase
+        text = text.lower()
+
+        # Remove URLs
+        text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)
+
+        # Remove punctuation and numbers
+        text = re.sub(r'\p{P}+|[\d]+', '', text)
+
+        # Tokenization
+        tokens = text.split()
+
+        # Stopword removal
+        if remove_stopwords:
+            tokens = [token for token in tokens if token not in stop_words]
+
+        # Lemmatization
+        if lemmatize:
+            tokens = [lemmatizer.lemmatize(token) for token in tokens]
+
+        processed_texts.append(" ".join(tokens))
+    
+    return processed_texts
